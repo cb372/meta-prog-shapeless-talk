e5074475233cc9234972ccf35282ef18c399bbec
Chris Birchall <chris.birchall@gmail.com>
2016-11-20 15:18:07 +0000

    Implement def macro echo example

--- macros/src/main/scala/Echo.scala
+++ macros/src/main/scala/Echo.scala
@@ -10,7 +10,15 @@ object Echo {
   def echo_impl[A](c: Context)(block: c.Tree): c.Tree = {
     import c.universe._
 
-    ???
+    println("Here's the code we were passed as an argument:")
+    println(showCode(block))
+    println("====")
+
+    println("Here's how the compiler sees it:")
+    println(prettyTree(c)(block))
+
+    // return the input unchanged
+    block
   }
 
 }
