a6dfeb75828eb1ad31cc339a9c7747789a58e94f
Chris Birchall <chris.birchall@gmail.com>
2016-11-27 19:00:53 +0000

    Implement meta paradise annotation example

--- scalameta/src/main/scala/Mustache.scala
+++ scalameta/src/main/scala/Mustache.scala
@@ -29,7 +29,16 @@ class Mustache(templateFile: String) extends StaticAnnotation {
       case q"object $name extends { ..$earlyInits } with ..$parentCtors { $param => ..$body }" =>
         val templateBody: String = loadTemplate(filename)
         val variableNames: Set[String] = TemplateParser.findVariableNames(templateBody)
-        ???
+        val ctorParams = variableNames.to[collection.immutable.Seq].sorted.map(name => param"${Term.Name(name)}: String")
+        q"""
+          {
+            object $name extends { ..$earlyInits } with ..$parentCtors { $param =>
+              val template: String = $templateBody
+              ..$body
+            }
+            case class ${Type.Name(name.toString)}(..$ctorParams)
+          }
+        """
 
       case _ => abort("You must annotate an object definition without an accompanying class.")
     }
